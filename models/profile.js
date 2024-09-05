82f319ecfa3bd5b8f5e049f20366f8140547de53
Sunny Master <master.codeworks@gmail.com>
2024-09-05 10:38:07 -0400

    add models and subschemas

--- models/profile.js
+++ models/profile.js
@@ -2,13 +2,51 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const genreSchema = new Schema ({
+  genreId: Number, 
+  genreName: String
+},{
+  timestamps: true,
+})
+
+const celebSchema = new Schema({
+  celebId: Number,
+  skill: String,
+  name: String,
+  imageUrl: String,
+  knownFor: [String],
+},{
+  timestamps: true,
+})
+
+const movieSchema = new Schema({
+  movieId: Number,
+  title: String,
+  genreIds: [Number],
+  plot: String,
+  rating: String,
+  imageUrl: String,
+  releaseDate: String,
+},{
+  timestamps: true,
+})
+
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  favGenres: [genreSchema],
+  favActors: [celebSchema],
+  favDirectors: [celebSchema],
+  favMovies: [movieSchema],
+  watchList: [movieSchema],
+  movieConcepts: [{ type: Schema.Types.ObjectId, ref: 'MovieConcept' }]
 },{
   timestamps: true,
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
 
-export { Profile }
+export { 
+  Profile,
+  celebSchema,  
+}
