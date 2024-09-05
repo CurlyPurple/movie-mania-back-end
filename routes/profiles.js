7be8dcf830471b23d008b18222de2716cc4c2aa1
Sunny Master <master.codeworks@gmail.com>
2024-09-05 17:33:46 -0400

    add route and controller for adding favorite actor to profile

--- routes/profiles.js
+++ routes/profiles.js
@@ -11,5 +11,7 @@ const router = Router()
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+router.post('/add-actor', checkAuth, profilesCtrl.addActor)
+
 
 export { router }
